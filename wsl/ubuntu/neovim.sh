c8365654bd2b6602ea44ddcde4e42299848574ce
y-knzw <60739587+y-knzw@users.noreply.github.com>
2022-05-28 21:50:18 +0900

    Add editor alternatives

--- wsl/ubuntu/neovim.sh
+++ wsl/ubuntu/neovim.sh
@@ -5,3 +5,10 @@ sudo apt install -y software-properties-common python3-software-properties
 sudo add-apt-repository -y ppa:neovim-ppa/stable
 sudo apt update
 sudo apt install -y neovim
+
+sudo update-alternatives --install /usr/bin/vi vi /usr/bin/nvim 60
+sudo update-alternatives --set vi /usr/bin/nvim
+sudo update-alternatives --install /usr/bin/vim vim /usr/bin/nvim 60
+sudo update-alternatives --set vim /usr/bin/nvim
+sudo update-alternatives --install /usr/bin/editor editor /usr/bin/nvim 60
+sudo update-alternatives --set editor /usr/bin/nvim
